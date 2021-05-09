2619ac3d6910f308927e5d649ef03f1b430ce277
szaszdani <szaszdani@gmail.com>
2021-05-09 13:53:18 +0200

    Login fixed
    Jelszó rész fixálva lett.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {LoginServiceService} from "../login-service.service";
-import {Router} from "@angular/router";
+import {LoginServiceService} from '../login-service.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',

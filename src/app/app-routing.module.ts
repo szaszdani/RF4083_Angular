35966ac656bf5b2ca94a2d6e414d5c8f390621e3
szaszdani <szaszdani@gmail.com>
2021-05-09 13:42:15 +0200

    Components added
    List, Form components hozzáadva.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {LoginGuardGuard} from './login-guard.guard';
+import {ListComponent} from './list/list.component';
+import {FormComponent} from './form/form.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'list',
+    component: ListComponent,
+    canActivate: [LoginGuardGuard]
+  },
+  {
+    path: 'form',
+    component: FormComponent,
+    canActivate: [LoginGuardGuard]
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import {MatButtonModule} from "@angular/material/button";
+import { ListComponent } from './list/list.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ListComponent,
+    FormComponent
   ],
     imports: [
         BrowserModule,
